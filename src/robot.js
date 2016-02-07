44e2aa23b8e7592eb04af072aff1a7abd6d2fa94
arunthampi <arun.thampi@gmail.com>
2016-02-07 13:20:03 -0800

    Add Robot#respondPattern method
    Ported from hubot

--- src/robot.js
+++ src/robot.js
@@ -12,4 +12,26 @@ var Robot = function(teamId, botId, debugMode) {
   this.debugMode = debugMode;
 };
 
+// Public: Build a regular expression that matches messages addressed
+// directly to the robot
+//
+// regex - A RegExp for the message part that follows the robot's name
+//
+// Returns RegExp.
+Robot.prototype.respondPattern = function(regex) {
+  var re = regex.toString().split('/');
+  re.shift();
+
+  var modifiers = re.pop();
+  if (re[0] && re[0][0] === '^') {
+    this.logger.warning("Anchors don't work well with respond, perhaps you want to use 'hear'");
+    this.logger.warning("The regex in question was " + (regex.toString()));
+  }
+
+  var pattern = re.join('/');
+
+  newRegex = new RegExp("[<]?[@]?" + this.botId + "(?:[^>]+>:)?\\s*(?:" + pattern + ")", modifiers);
+  return newRegex;
+};
+
 module.exports = Robot;
---
+++ test/robot_test.js
@@ -0,0 +1,45 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var Robot = require('../src/robot');
+
+chai.use(require('sinon-chai'));
+var expect = chai.expect;
+
+describe('Robot', function() {
+  beforeEach(function() {
+    this.robot = new Robot('TDEADBEEF', 'UNESTORBOT1', false);
+  })
+
+  describe('Unit Tests', function() {
+    describe('#respondPattern', function() {
+      it('matches messages starting with robot\'s name', function() {
+        var testMessage = this.robot.botId + ' message123';
+        var testRegex = /(.*)/;
+        var pattern = this.robot.respondPattern(testRegex);
+        expect(testMessage).to.match(pattern);
+
+        var match = testMessage.match(pattern)[1];
+        expect(match).to.equal('message123');
+      });
+
+      it('matches messages starting with robot\'s name (in Slack style)', function() {
+        var testMessage = "<@" + this.robot.botId + "|nestorbot>: " + 'message123';
+        var testRegex = /(.*)/;
+        var pattern = this.robot.respondPattern(testRegex);
+        expect(testMessage).to.match(pattern);
+
+        var match = testMessage.match(pattern)[1];
+        expect(match).to.equal('message123');
+      });
+
+      it('does not match unaddressed messages', function() {
+        var pattern, testMessage, testRegex;
+        testMessage = 'message123';
+        testRegex = /(.*)/;
+        pattern = this.robot.respondPattern(testRegex);
+        expect(testMessage).to.not.match(pattern);
+      });
+    });
+  });
+});
+
