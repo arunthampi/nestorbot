7607b0cbc15bf5621f0e5cb280259ce7838e1fbb
arunthampi <arun.thampi@gmail.com>
2016-02-08 18:48:29 -0800

    Add Robot#http
    ported from Hubot

--- src/robot.js
+++ src/robot.js
@@ -1,9 +1,14 @@
 var _ref = require('./listener'), Listener = _ref.Listener, TextListener = _ref.TextListener;
 var TextMessage = require('./message').TextMessage;
+var HttpClient = require('scoped-http-client');
 var Response = require('./response');
 var Path = require('path');
 var Log = require('log');
 var Promise = require('bluebird');
+
+var __hasProp = {}.hasOwnProperty,
+    __slice = [].slice,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
 // Robots receive messages from Nestor and dispatch them to matching listeners.
 //
@@ -20,6 +25,7 @@ var Robot = function(teamId, botId, debugMode) {
   this.listeners = [];
   this.toSend = [];
   this.toReply = [];
+  this.globalHttpOptions = {};
 
   if(this.debugMode == true) {
     process.env.NESTOR_LOG_LEVEL = 'debug';
@@ -101,6 +107,42 @@ Robot.prototype.loadFile = function(path, file) {
       return process.exit(1);
     }
   }
+};
+
+// Public: Creates a scoped http client with chainable methods for
+// modifying the request. This doesn't actually make a request though.
+// Once your request is assembled, you can call `get()`/`post()`/etc to
+// send the request.
+//
+// url - String URL to access.
+// options - Optional options to pass on to the client
+//
+// Examples:
+//
+//     robot.http("http://example.com")
+//       # set a single header
+//       .header('Authorization', 'bearer abcdef')
+//
+//       # set multiple headers
+//       .headers(Authorization: 'bearer abcdef', Accept: 'application/json')
+//
+//       # add URI query parameters
+//       .query(a: 1, b: 'foo & bar')
+//
+//       # make the actual request
+//       .get() (err, res, body) ->
+//         console.log body
+//
+//       # or, you can POST data
+//       .post(data) (err, res, body) ->
+//         console.log body
+//
+//    # Can also set options
+//    robot.http("https://example.com", {rejectUnauthorized: false})
+//
+// Returns a ScopedClient instance.
+Robot.prototype.http = function(url, options) {
+  return HttpClient.create(url, this.extend({}, this.globalHttpOptions, options)).header('User-Agent', "Nestorbot/" + this.version);
 };
 
 // Public: Passes the given message to any interested Listeners
