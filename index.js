c731a2b6d94da7a771ce1284b4b7dd934d7d5133
arunthampi <arun.thampi@gmail.com>
2016-02-08 20:32:13 -0800

    Export all required functions

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var Robot = require('./src/robot');
+var Response = require('./src/response');
+var _ref = require('./src/message'), Message = _ref.Message, TextMessage = _ref.TextMessage;
+var User = require('./src/user');
 
 module.exports = {
-  Robot: Robot
+  Robot: Robot,
+  Response: Response,
+  Message: Message,
+  TextMessage: TextMessage,
+  User: User
 }
